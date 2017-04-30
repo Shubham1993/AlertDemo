26e2bc4774428d85a2eb9569812af28863b2c2d1
Shubham <shubham@shubhams-mac.local>
2017-05-01 00:47:06 +0530

    alert Demo

--- AlertDemo/ViewController.m
+++ AlertDemo/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    UIAlertController *alert;
+}
 
 @end
 
@@ -22,6 +25,34 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (IBAction)alert:(id)sender {
+    
+    alert = [UIAlertController alertControllerWithTitle:@"Alert!" message:@"This is an alert..." preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *action = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        
+    }];
+    [alert addAction:action];
+    
+    [self presentViewController:alert animated:YES completion:nil];
+    
+}
+
+
+- (IBAction)alertWithTimer:(id)sender {
+    
+    alert = [UIAlertController alertControllerWithTitle:@"Alert!" message:@"This is an alert..." preferredStyle:UIAlertControllerStyleAlert];
+    NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:5.0 target:self selector:@selector(dismissAlert) userInfo:nil repeats:NO];
+    
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
+
+-(void)dismissAlert{
+    
+    [alert dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
